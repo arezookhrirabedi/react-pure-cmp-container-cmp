c23a188cc7e9b464081d7e893568d3434e86d4f0
arezookhrirabedi <a_kheirabadi83@yahoo.com>
2021-01-31 20:39:05 +0330

    redux

--- src/component/app/App.tsx
+++ src/component/app/App.tsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+import { Store } from '../../redux/store'
+import { User } from '../pages/user/user'
 
 export class App extends Component {
   render() {
     return (
       <div>
-        
+        <Provider store={Store}>
+          <User/>
+          helooooo
+        </Provider>
       </div>
     )
   }
--- src/component/pages/user/user.tsx
+++ src/component/pages/user/user.tsx
@@ -1,11 +1,41 @@
-import React, { Component } from 'react'
-
-export default class User extends Component {
+import React from 'react'
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
+import { IUser } from '../../../model/model.user';
+import { action_updateUsers } from '../../../redux/action/users';
+import { IReduxState } from '../../../redux/app.state';
+interface IProps{
+    users:Array<IUser>}
+    interface IState{}
+ class UserComponent extends React.Component<IProps,IState> {
     render() {
+        alert(this.props.users)
         return (
+
             <div>
-              user  
+              {this.props.users.map((user:IUser,index:number)=>{
+                  return(
+                    <>
+                    {/* <div key={index}>
+                        {user}
+                    </div> */}
+                    </>
+                  )
+              })} 
             </div>
         )
     }
 }
+const mapDispatchToProps = (dispatch: Dispatch) => {
+    return {
+      onUpdateUsers: (users: Array<IUser>) => dispatch(action_updateUsers(users)),
+    };
+  };
+  const mapStateToProps = (state: IReduxState) => {
+    return {
+      users: state.users,
+    };
+  };
+  
+  
+export const User= connect(mapStateToProps, mapDispatchToProps)(UserComponent );
